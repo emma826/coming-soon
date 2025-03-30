b09503046b4f2ef5aa4edfe59dd65d16e2e0b6cf
Amoke Emmanuel <emmanuelamoke35@gmail.com>
2025-03-30 12:50:13 +0100

    second commit

---
+++ src/app/api/contacts/route.js
@@ -0,0 +1,51 @@
+import fetch from 'node-fetch';
+
+export async function POST(req) {
+    const HUBSPOT_API_URL = 'https://api.hubapi.com/crm/v3/objects/contacts';
+    const HUBSPOT_API_KEY = process.env.HUBSPOT_API_KEY; // Ensure you have this in your environment variables
+
+    if (!HUBSPOT_API_KEY) {
+        return new Response(JSON.stringify({ error: 'HubSpot API key is missing' }), { status: 500 });
+    }
+
+    const createContactInHubSpot = async (contactData) => {
+        const response = await fetch(HUBSPOT_API_URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${HUBSPOT_API_KEY}`,
+            },
+            body: JSON.stringify(contactData),
+        });
+
+        if (!response.ok) {
+            console.error(response)
+            return { error: true, data: response.statusText }
+            // throw new Error(`Failed to create contact: ${response.statusText}`);
+        }
+
+        return { success: true, data: response.json() }
+    };
+
+    try {
+        const { firstname, lastname, email } = await req.json();
+        const contactData = {
+            properties: {
+                firstname,
+                lastname,
+                email,
+            },
+        };
+        const { error, success, data } = await createContactInHubSpot(contactData);
+
+        if (success) {
+            return new Response(JSON.stringify(data), { status: 200 });
+        }
+
+        throw new Error(data)
+
+    } catch (error) {
+        console.error('Error creating contact:', error);
+        return new Response(JSON.stringify({ error: error.message }), { status: 500 });
+    }
+}
--- src/app/page.js
+++ src/app/page.js
@@ -8,12 +8,78 @@ import {
 	CardHeader,
 	CardTitle,
 } from "@/components/ui/card"
-
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+
+import { useState } from "react";
 
 export default function Home() {
+
+	const [first_name, set_first_name] = useState("")
+	const [last_name, set_last_name] = useState("")
+	const [email, set_email] = useState("")
+	const [alertMessage, setAlertMessage] = useState("");
+	const [alertType, setAlertType] = useState("destructive"); // "destructive", "outline", or "destructive"
+
+	async function handleSubmit() {
+		if (first_name === "" || last_name === "" || email === "") {
+			setAlertMessage("Please fill in all fields");
+			setAlertType("destructive");
+
+			setTimeout(() => {
+				setAlertMessage("");
+			}, 1000);
+
+			return;
+		}
+
+		try {
+			const res = await fetch("/api/contacts", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({
+					firstname: first_name.trim(),
+					lastname: last_name.trim(),
+					email: email.trim(),
+				}),
+			});
+
+			const data = await res.json();
+			if (res.status === 200) {
+				setAlertMessage("You have successfully joined the waitlist");
+				setAlertType("outline");
+				set_first_name("");
+				set_last_name("");
+				set_email("");
+
+				setTimeout(() => {
+					setAlertMessage("");
+				}, 1000);
+			} else if (res.status === 409) {
+				setAlertMessage(data.message || "A contact with this email already exists.");
+				setAlertType("destructive");
+
+				setTimeout(() => {
+					setAlertMessage("");
+				}, 1000);
+			} else {
+				setAlertMessage(data.message || "An error occurred. Please try again.");
+				setAlertType("destructive");
+
+				setTimeout(() => {
+					setAlertMessage("");
+				}, 1000);
+			}
+		} catch (error) {
+			setAlertMessage("An error occurred. Please try again later.");
+			setAlertType("destructive");
+		}
+	}
+
 	return (
 		<div className="h-screen">
 			<video
@@ -39,13 +105,35 @@ export default function Home() {
 							<CardTitle>Join the Waitlist & Get Exclusive Early Access!</CardTitle>
 							<CardDescription>You can also follow Fiuzar on social media by clicking on the link below</CardDescription>
 						</CardHeader>
-						<CardContent>
+
+						{/* {alertMessage && (
+							<Alert variant={alertType}>
+								<AlertTitle>{alertType === "outline" ? "Success" : "Error"}</AlertTitle>
+								<AlertDescription>
+									{alertMessage}
+								</AlertDescription>
+							</Alert>
+						)} */}
+
+						<div className="px-4 pr-6">
+							{alertMessage && (
+								<Alert variant={alertType} className={`mx-auto`}>
+									<AlertDescription>
+										{alertMessage}
+									</AlertDescription>
+								</Alert>
+							)}
+						</div>
+
+						<CardContent className={`-mt-4`}>
 							<div className="flex flex-col gap-2 mb-2">
 								<Label htmlFor="email">First Name</Label>
 								<Input
 									id="first_name"
 									type="text"
 									placeholder="Enter your first name"
+									value={first_name}
+									onChange={(e) => set_first_name(e.target.value)}
 								/>
 							</div>
 							<div className="flex flex-col gap-2 mb-2">
@@ -54,6 +142,8 @@ export default function Home() {
 									id="last_name"
 									type="text"
 									placeholder="Enter your last name"
+									value={last_name}
+									onChange={(e) => set_last_name(e.target.value)}
 								/>
 							</div>
 							<div className="flex flex-col gap-2 mb-2">
@@ -62,16 +152,17 @@ export default function Home() {
 									id="email"
 									type="email"
 									placeholder="Enter your email address"
-									required
+									value={email}
+									onChange={(e) => set_email(e.target.value)}
 								/>
 							</div>
-							<Button className={`py-6 w-full max-w-44 mt-3`}>Join</Button>
+							<Button className={`py-6 w-full max-w-44 mt-3`} onClick={handleSubmit}>Join</Button>
 						</CardContent>
 					</Card>
-					
+
 				</div>
 
-			
+
 			</div>
 		</div>
 	);
